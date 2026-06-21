ea98cd0ca99df1f189ea59b5a92131bea949d75a
12Harris <harris.hassell@t-online.de>
2026-06-22 01:14:00 +0200

    preliminary build
    preliminary build

--- scripts/Game_Manager.gd
+++ scripts/Game_Manager.gd
@@ -55,7 +55,7 @@ func register_dog_owner(do:DogDowner) ->void:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
-		
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if !initialized:
